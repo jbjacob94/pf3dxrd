98d66dc830221309b609697a3cd6e954aaec97d9
Jean-Baptiste Jacob <137695414+jbjacob94@users.noreply.github.com>
2025-11-07 14:30:47 +0100

    Update pixelmap.py

--- pf3dxrd/pixelmap.py
+++ pf3dxrd/pixelmap.py
@@ -17,7 +17,7 @@ import xfab
 
 from orix import data, io, plot as opl, quaternion as oq, vector as ovec
 
-from pf_3dxrd import utils, crystal_structure, peak_mapping
+from pf3dxrd import utils, crystal_structure, peak_mapping
 
 """
 plot scanning 3DXRD outputs on a 2D pixelmap. 
